fddfe3d765e2ab5c1f504be5488580463183f31f
Amir <mokarchi@gmail.com>
2020-10-26 12:58:59 +0330

    Implementation of the Category Service

---
+++ src/Core/Mok.Blog/Services/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Mok.Blog.Data;
+using Mok.Blog.Models;
+using Mok.Blog.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository categoryRepository;
+        private readonly ILogger<CategoryService> logger;
+        public CategoryService(ICategoryRepository categoryRepository,
+                               ILogger<CategoryService> logger)
+        {
+            this.categoryRepository = categoryRepository;
+            this.logger = logger;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await categoryRepository.GetListAsync();
+        }
+    }
+}
---
+++ src/Core/Mok.Blog/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using Mok.Blog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        /// <summary>
+        /// Returns all categories.
+        /// </summary>
+        Task<List<Category>> GetAllAsync();
+    }
+}
