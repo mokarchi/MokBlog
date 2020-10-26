68ebb4bdfe032254afda93e2a6774e42a112d23f
Amir <mokarchi@gmail.com>
2020-10-26 12:51:44 +0330

    implementation of the Category Repository

---
+++ src/Core/Mok.Blog/Data/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mok.Blog.Models;
+using Mok.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Data
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        /// <summary>
+        /// Returns a list of <see cref="Category"/>, the returned objects are not tracked.
+        /// The returned list are order by alphabetically on <see cref="Category.Title"/>.
+        /// </summary>
+        Task<List<Category>> GetListAsync();
+    }
+}
