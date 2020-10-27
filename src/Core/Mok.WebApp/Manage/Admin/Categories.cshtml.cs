ae65866649edd51930160951df34a91b01e2207f
Amir <mokarchi@gmail.com>
2020-10-27 09:30:52 +0330

    get category and show on ui layer

--- src/Core/Mok.WebApp/Manage/Admin/Categories.cshtml.cs
+++ src/Core/Mok.WebApp/Manage/Admin/Categories.cshtml.cs
@@ -1,16 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mok.Blog.Services.Interfaces;
+using Newtonsoft.Json;
 
 namespace Mok.WebApp.Manage.Admin
 {
     public class CategoriesModel : PageModel
     {
-        public void OnGet()
+        private readonly ICategoryService _catSvc;
+
+        public CategoriesModel(ICategoryService catService)
         {
+            _catSvc = catService;
+        }
+
+        public string CategoryListJsonStr { get; private set; }
+
+        /// <summary>
+        /// GET
+        /// </summary>
+        public async Task OnGetAsync()
+        {
+            var cat = await _catSvc.GetAllAsync();
+            CategoryListJsonStr = JsonConvert.SerializeObject(cat);
         }
     }
 }
