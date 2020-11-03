281a330d86f3d0279ff1b7860c815cad43410c89
Amir <mokarchi@gmail.com>
2020-11-03 18:18:19 +0330

    Blog cache keys and intervals.

---
+++ src/Core/Mok.Blog/Helpers/BlogCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mok.Blog.Helpers
+{
+    /// <summary>
+    /// Blog cache keys and intervals.
+    /// </summary>
+    public class BlogCache
+    {
+        public const string KEY_ALL_CATS = "BlogCategories";
+
+        /// <summary>
+        /// 10 minutes.
+        /// </summary>
+        public static readonly TimeSpan Time_AllCats = new TimeSpan(0, 10, 0);
+    }
+}
