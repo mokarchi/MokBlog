4c4fc143870606c14e415c0d28f00cc9f5d5807c
Amir <mokarchi@gmail.com>
2020-11-03 20:51:57 +0330

    complete unit test for category services

--- test/Mok.Blog.Tests/Services/CategoryServiceTest.cs
+++ test/Mok.Blog.Tests/Services/CategoryServiceTest.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Mok.Blog.Data;
+using Mok.Blog.Helpers;
 using Mok.Blog.Models;
 using Mok.Blog.Services;
 using Mok.Blog.Services.Interfaces;
@@ -59,6 +60,84 @@ namespace Mok.Blog.Tests.Services
             var ex = await Assert.ThrowsAsync<MokException>(() => categoryService.CreateAsync(title));
 
             Assert.Equal("'web development' already exists.", ex.Message);
+        }
+
+        /// <summary>
+        /// Test <see cref="CategoryService.CreateCategoryAsync(Category)"/> would call Repository's 
+        /// CreateAsync and invalidates cache for all categories.
+        /// </summary>
+        [Fact]
+        public async void Create_category_calls_repo_and_invalidates_cache_for_all_categories()
+        {
+            // Arrange 
+            var cat = new Category { Title = "Cat1" };
+
+            // Act
+            await categoryService.CreateAsync(cat.Title);
+
+            // Assert
+            catRepoMock.Verify(repo => repo.CreateAsync(It.IsAny<Category>()), Times.Exactly(1));
+            Assert.Null(await cache.GetAsync(BlogCache.KEY_ALL_CATS));
+        }
+
+        /// <summary>
+        /// Delete category calls repository and invalidates cache for all categories.
+        /// </summary>
+        /// <remarks>
+        /// This test depends on Mock<ISettingService> to provide BlogSettings. 
+        /// </remarks>
+        [Fact]
+        public async void Delete_category_calls_repo_and_invalidates_cache_for_all_categories()
+        {
+            // Act
+            await categoryService.DeleteAsync(2);
+
+            // Assert
+            catRepoMock.Verify(repo => repo.DeleteAsync(It.IsAny<int>(), It.IsAny<int>()), Times.Exactly(1));
+            Assert.Null(await cache.GetAsync(BlogCache.KEY_ALL_CATS));
+        }
+
+        /// <summary>
+        /// Update category would call CategoryRepository and then invalidates cache for all categories.
+        /// </summary>
+        [Fact]
+        public async void Update_category_calls_repo_and_invalidates_cache_for_all_categories()
+        {
+            // Arrange
+            var cat = await categoryService.GetAsync(1);
+
+            // Act
+            cat.Title = "Cat1";
+            await categoryService.UpdateAsync(cat);
+
+            // Assert
+            catRepoMock.Verify(repo => repo.UpdateAsync(It.IsAny<Category>()), Times.Exactly(1));
+            Assert.Null(await cache.GetAsync(BlogCache.KEY_ALL_CATS));
+        }
+
+        /// <summary>
+        /// Category needs a valid id and title to update.
+        /// </summary>
+        [Fact]
+        public async void Update_category_with_invalid_category_throws_MokException()
+        {
+            await Assert.ThrowsAsync<MokException>(() => categoryService.UpdateAsync(null));
+            await Assert.ThrowsAsync<MokException>(() => categoryService.UpdateAsync(new Category())); // invalid id and title
+            await Assert.ThrowsAsync<MokException>(() => categoryService.UpdateAsync(new Category { Id = 1 })); // invalid title
+        }
+
+        
+        [Fact]
+        public async void Update_category_with_title_changed_only_in_casing_is_OK()
+        {
+            var cat = await categoryService.GetAsync(1);
+            Assert.Equal("Web Development", cat.Title);
+
+            cat.Title = "web development";
+
+            var catAgain = await categoryService.UpdateAsync(cat);
+            Assert.Equal(1, catAgain.Id);
+            Assert.Equal("web development", catAgain.Title);
         }
     }
 }
