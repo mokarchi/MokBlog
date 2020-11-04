e16f1349b527f8c55d6d2dc7b36fced19974b0d0
Amir <mokarchi@gmail.com>
2020-11-04 12:50:57 +0330

    Sql implementation of the IMetaRepository contract

---
+++ src/Core/Mok/Data/EMetaType.cs
@@ -0,0 +1,13 @@
+﻿namespace Mok.Data
+{
+    /// <summary>
+    /// The different meta types.
+    /// </summary>
+    public enum EMetaType
+    {
+        /// <summary>
+        /// A setting.
+        /// </summary>
+        Setting = 0
+    }
+}
