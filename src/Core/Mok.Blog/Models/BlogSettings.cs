9fc7d6fab7cbf55750fbf9447660d5b4457f7c3f
Amir <mokarchi@gmail.com>
2020-11-04 12:52:59 +0330

    Settings for the blog application

---
+++ src/Core/Mok.Blog/Models/BlogSettings.cs
@@ -0,0 +1,18 @@
+﻿using Mok.Settings;
+
+namespace Mok.Blog.Models
+{
+    /// <summary>
+    /// Settings for the blog application.
+    /// </summary>
+    /// <remarks>
+    /// This is saved in the meta table, it's ok to add or remove properties here.
+    /// </remarks>
+    public class BlogSettings : ISettings
+    {
+        /// <summary>
+        /// There must be one default category. Default 1.
+        /// </summary>
+        public int DefaultCategoryId { get; set; } = 1;
+    }
+}
