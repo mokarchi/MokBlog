4ef56ff13c2daaf55b8a5cc4d7cb568d6d5ff50d
Amir <mokarchi@gmail.com>
2020-11-04 12:49:07 +0330

    A key value pair record

---
+++ src/Core/Mok/Data/CoreEntityModelBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MokCore.Data;
+
+namespace Mok.Data
+{
+    /// <summary>
+    /// The core entity model.
+    /// </summary>
+    public class CoreEntityModelBuilder : IEntityModelBuilder
+    {
+        public void CreateModel(ModelBuilder builder)
+        {
+            builder.Entity<Meta>(entity =>
+            {
+                entity.ToTable("Core_Meta");
+                entity.HasKey(e => e.Id).IsClustered(clustered: false);
+                entity.HasIndex(e => new { e.Type, e.Key }).IsUnique().IsClustered();
+            });
+        }
+    }
+}
