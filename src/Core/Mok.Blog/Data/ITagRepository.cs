2d779d0bd2ead1dc90c62426e3deb9172329a088
Amir <mokarchi@gmail.com>
2020-11-07 16:29:05 +0330

    Sql implementation

---
+++ src/Core/Mok.Blog/Data/ITagRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mok.Blog.Models;
+using Mok.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Data
+{
+    public interface ITagRepository : IRepository<Tag>
+    {
+        /// <summary>
+        /// Returns all tags or empty list if no tags found. The returned list is ordered by 
+        /// <see cref="Tag.Count"/> desc.
+        /// </summary>
+        Task<List<Tag>> GetListAsync();
+    }
+}
---
+++ src/Core/Mok.Blog/Data/SqlTagRepository.cs
@@ -0,0 +1,37 @@
+﻿using Mok.Blog.Models;
+using Mok.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Data
+{
+    /// <summary>
+    /// Sql implementation of the <see cref="ITagRepository"/> contract.
+    /// </summary>
+    public class SqlTagRepository : EntityRepository<Tag>, ITagRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public SqlTagRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Returns all tags or empty list if no tags found. The returned list is ordered by 
+        /// <see cref="Tag.Count"/> desc and then by <see cref="Tag.Title"/>.
+        /// </summary>
+        public async Task<List<Tag>> GetListAsync()
+        {
+            return await (from t in _entities
+                          select new Tag
+                          {
+                              Id = t.Id,
+                              Title = t.Title,
+                              Slug = t.Slug,
+                              Description = t.Description
+                          }).ToListAsync();
+        }
+    }
+}
