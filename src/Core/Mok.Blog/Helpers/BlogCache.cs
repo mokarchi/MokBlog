2aae84b545e68e238b588af889ee5bb2f073043f
Amir <mokarchi@gmail.com>
2020-11-07 16:31:30 +0330

    Tag cache keys and intervals.

--- src/Core/Mok.Blog/Helpers/BlogCache.cs
+++ src/Core/Mok.Blog/Helpers/BlogCache.cs
@@ -10,10 +10,16 @@ namespace Mok.Blog.Helpers
     public class BlogCache
     {
         public const string KEY_ALL_CATS = "BlogCategories";
+        public const string KEY_ALL_TAGS = "BlogTags";
 
         /// <summary>
         /// 10 minutes.
         /// </summary>
         public static readonly TimeSpan Time_AllCats = new TimeSpan(0, 10, 0);
+
+        /// <summary>
+        /// 10 minutes.
+        /// </summary>
+        public static readonly TimeSpan Time_AllTags = new TimeSpan(0, 10, 0);
     }
 }
