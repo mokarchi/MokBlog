87d8ddbacbcbcc0d50dd582803f1fe6a9a07f25e
Amir <mokarchi@gmail.com>
2020-11-07 16:30:35 +0330

    The blog tag service.

---
+++ src/Core/Mok.Blog/Services/Interfaces/ITagService.cs
@@ -0,0 +1,15 @@
+﻿using Mok.Blog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Services.Interfaces
+{
+    public interface ITagService
+    {
+        /// <summary>
+        /// Returns all tags, cached after calls to DAL.
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Tag>> GetAllAsync();
+    }
+}
---
+++ src/Core/Mok.Blog/Services/TagService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Mok.Blog.Data;
+using Mok.Blog.Helpers;
+using Mok.Blog.Models;
+using Mok.Blog.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mok.Blog.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly ITagRepository tagRepository;
+        private readonly IDistributedCache cache;
+        private readonly ILogger<TagService> logger;
+
+        public TagService(ITagRepository tagRepository,
+                          IDistributedCache cache,
+                          ILogger<TagService> logger)
+        {
+            this.tagRepository = tagRepository;
+            this.cache = cache;
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Returns all tags, cached after calls to DAL.
+        /// </summary>
+        public async Task<List<Tag>> GetAllAsync()
+        {
+            return await cache.GetAsync(BlogCache.KEY_ALL_TAGS, BlogCache.Time_AllTags, async () => {
+                return await tagRepository.GetListAsync();
+            });
+        }
+    }
+}
