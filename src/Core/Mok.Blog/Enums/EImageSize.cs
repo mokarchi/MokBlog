3e9d206e04a6ca0543a7b3ac9215e8a93c0649c6
Amir <mokarchi@gmail.com>
2020-11-14 22:47:24 +0330

    Blog image sizes enum

---
+++ src/Core/Mok.Blog/Enums/EImageSize.cs
@@ -0,0 +1,14 @@
+﻿namespace Mok.Blog.Enums
+{
+    /// <summary>
+    /// Blog image sizes.
+    /// </summary>
+    public enum EImageSize
+    {
+        Original,
+        Large,
+        MediumLarge,
+        Medium,
+        Small,
+    }
+}
