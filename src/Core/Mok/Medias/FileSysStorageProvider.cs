ae8081543081f10233dcc8d674748a7b63b1d686
Amir <mokarchi@gmail.com>
2020-11-16 11:57:42 +0330

    File system storage

---
+++ src/Core/Mok/Medias/FileSysStorageProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Mok.Medias
+{
+    /// <summary>
+    /// File system storage.
+    /// </summary>
+    /// user uploads file with an existing name, get a unique name
+    /// the problem is olw, if user resizes an image, be aware olw sends it as new file
+    /// also olw each time sends two copies of the file, orig and thumb
+    public class FileSysStorageProvider : IStorageProvider
+    {
+        private readonly HttpRequest _request;
+        
+        public FileSysStorageProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _request = httpContextAccessor.HttpContext.Request;
+        }
+        /// <summary>
+        /// The absolute URI endpoint to file, e.g. "https://localhost:44381" or "https://www.fanray.com".
+        /// </summary>
+        public string StorageEndpoint => $"{_request.Scheme}://{_request.Host}{_request.PathBase}";
+    }
+}
---
+++ src/Core/Mok/Medias/IStorageProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace Mok.Medias
+{
+    /// <summary>
+    /// The storage provider save the incoming file whether its byte[] or stream into the storage.
+    /// It makes sure it gets a unique filename by looking at what is already in storage.
+    /// </summary>
+    public interface IStorageProvider
+    {
+        /// <summary>
+        /// The absolute URI endpoint to resource.
+        /// </summary>
+        string StorageEndpoint { get; }
+    }
+}
