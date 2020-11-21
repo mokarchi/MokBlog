743bfaceec56cd48117dcfd1a9f8fd6deeb5a687
Amir <mokarchi@gmail.com>
2020-11-21 19:55:50 +0330

    create core settings for the site

---
+++ src/Core/Mok/Settings/CoreSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mok.Settings
+{
+    /// <summary>
+    /// Core settings for the site.
+    /// </summary>
+    public class CoreSettings : ISettings
+    {
+        /// <summary>
+        /// Default theme "Clarity".
+        /// </summary>
+        /// <remarks>
+        /// This is the theme's folder name.
+        /// </remarks>
+        public string Theme { get; set; } = "Clarity";
+
+        /// <summary>
+        /// Gets or sets the timezone id for the site. Default "UTC".
+        /// </summary>
+        /// <remarks>
+        /// To learn more about timezone id <see cref="System.TimeZoneInfo.Id"/> and <see cref="http://stackoverflow.com/a/7908482/32240"/>
+        /// </remarks>
+        public string TimeZoneId { get; set; } = "UTC";
+    }
+}
