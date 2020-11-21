b58a826c64dd03c9da62785ccebfd52b37e460cf
Amir <mokarchi@gmail.com>
2020-11-21 19:56:42 +0330

    Converts a DateTimeOffset from the server to a user's local time with his specific timezone.

---
+++ src/Core/Mok/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TimeZoneConverter;
+
+namespace system
+{
+    public static class DateTimeOffsetExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="DateTimeOffset"/> from the server to a user's local time with 
+        /// his specific timezone.
+        /// </summary>
+        /// <param name="serverTime"></param>
+        /// <param name="timeZoneId">The timezone to convert server time to.</param>
+        /// <remarks>
+        /// Posts are saved Utc time on server and are converted to <paramref name="timeZoneId"/>
+        /// before displayed on the client.
+        /// </remarks>
+        public static DateTimeOffset ToLocalTime(this DateTimeOffset serverTime, string timeZoneId)
+        {
+            var userTimeZone = TZConvert.GetTimeZoneInfo(timeZoneId);
+            return TimeZoneInfo.ConvertTime(serverTime, userTimeZone);
+        }
+    }
+}
